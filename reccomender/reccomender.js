2467101d7c7df265802ca02960fab6f7ce4cb9d5
vikiru <72267229+vikiru@users.noreply.github.com>
2024-02-11 15:29:57 -0500

    Commit recent changes
    - need to obtain missing data from jikanapi (4510 entries or so)
    - need to normalize the animeentry class properties
    - group known scores into a user-interaction matrix?
    - train model with normalized + user interaction matrix
    - experiment with different similarity measures
    - reccomend 10 unique anime per input title

--- reccomender/reccomender.js
+++ reccomender/reccomender.js
@@ -1,27 +1,48 @@
 const fs = require("fs");
+const tf = require('@tensorflow/tfjs');
 const { parse } = require("csv-parse");
 const { AnimeEntry } = require("./AnimeEntry");
+
+
 const entries = [];
 
-fs.createReadStream("./data.csv")
-  .pipe(parse({ delimiter: ",", from_line: 2 }))
-  .on("data", function (row) {
-     const id = row[0];
-     const title = row[1];
-     const rank = row[2];
-     const type = row[3];
-     const episodeCount = row[4];
-     const aired = row[5];
-     const members = row[6];
-     const malURL = row[7];
-     const imageURL = row[8];
-     const score = row[9];
-     const animeEntry = new AnimeEntry(id, title, rank, type, episodeCount, aired, members, malURL, imageURL, score);
-     entries.push(animeEntry);
+const readAndParseCSV = () => {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream("./data.csv")
+      .pipe(parse({ delimiter: ",", from_line:  2 }))
+      .on("data", function (row) {
+         const id = row[0];
+         const title = row[1];
+         const rank = row[2];
+         const type = row[3];
+         const episodeCount = row[4];
+         const aired = row[5];
+         const members = row[6];
+         const malURL = row[7];
+         const imageURL = row[8];
+         const score = row[9];
+         const animeEntry = new AnimeEntry(id, title, rank, type, episodeCount, aired, members, malURL, imageURL, score);
+         if (type === 'TV'){
+            entries.push(animeEntry);
+         }
+      })
+      .on("end", resolve)
+      .on("error", reject);
+  });
+};
+
+readAndParseCSV()
+  .then(() => {
+    console.log(entries.length);
   })
-  .on("end", function () {
-    console.log(entries.length);  })
-  .on("error", function (error) {
-    console.log(error.message);
+  .catch((error) => {
+    console.error("Error reading and parsing CSV:", error);
   });
 
+
+
+  // initially content based filtering, then allow for collaborative approach based on user input
+  // normalize certain properties, one hot encode genres, etc..
+  // overlap similarity, jaccard similarity, cosine similarity,
+  // k means, cosine similarity, unsupervised learning, content, collaborative filter, hybrid, get missing data from JikanAPI.
+  // missing data: trailer, english/japanese title, source?, aired (update to use timestamp), scoredBy, popularity, favorites, studios, season, year, genres, 
