ba11a898ec4a9122a3aa1a54ce34ff41779f0122
vikiru <72267229+vikiru@users.noreply.github.com>
2024-02-12 21:21:59 -0500

    Update AnimeEntry class properties

--- reccomender/AnimeEntry.js
+++ reccomender/AnimeEntry.js
@@ -1,15 +1,30 @@
-class AnimeEntry{
-    constructor(id, title, rank, type, episodeCount, aired, members, malURL, imageURL, score){
+class AnimeEntry {
+    constructor(id, title, englishName, otherName, score, genres, synopsis, type, episodes, aired, premiered, status, producers, licensors, studios, source, duration, rating, rank, popularity, favourites, scoredBy, members, imageURL, pageURL) {
         this.id = id;
         this.title = title;
-        this.rank = rank;
-        this.type = type;
-        this.episodeCount = episodeCount;
-        this.aired = aired;
-        this.members = members;
-        this.malURL = malURL;
-        this.imageURL = imageURL;
+        this.englishName = englishName;
+        this.otherName = otherName;
         this.score = score;
+        this.genres = genres;
+        this.synopsis = synopsis;
+        this.type = type;
+        this.episodes = episodes;
+        this.aired = aired;
+        this.premiered = premiered;
+        this.status = status;
+        this.producers = producers;
+        this.licensors = licensors;
+        this.studios = studios;
+        this.source = source;
+        this.duration = duration;
+        this.rating = rating;
+        this.rank = rank;
+        this.popularity = popularity;
+        this.favourites = favourites;
+        this.scoredBy = scoredBy;
+        this.members = members;
+        this.imageURL = imageURL;
+        this.pageURL = pageURL;
     }
 }
 
