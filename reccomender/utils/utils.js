52f6930085741045fc6a78d41b9266c174cddbbb
vikiru <72267229+vikiru@users.noreply.github.com>
2024-02-13 16:23:20 -0500

    Create function to get season and year from either premiered or aired date

--- reccomender/utils/utils.js
+++ reccomender/utils/utils.js
@@ -16,6 +16,51 @@ function findMin(data, property) {
     return Math.min(...propArr);
 }
 
+const seasons = [
+    { name: "fall", months: ["Oct", "Nov", "Dec"] },
+    { name: "spring", months: ["Apr", "May", "Jun"] },
+    { name: "summer", months: ["Jul", "Aug", "Sep"] },
+    { name: "winter", months: ["Jan", "Feb", "Mar"] }
+  ];
+
+function cleanAired(text){
+    if (text === 'Not available'){
+        return {
+            season: 'Unknown',
+            year: 'Unknown',
+        }
+    }
+    else {
+        const regex = /([a-zA-z]+)\s+(\d+),?\s+(\d+)/im;
+        const matches = text.match(regex);
+        if (matches){
+            const matchLength = matches.length;
+            if (matchLength >= 2){
+                const month = matches[1];
+                const seasonMatch = seasons.find(season => season.months.includes(month));
+                let season = 'Unknown';
+                if (seasonMatch){
+                    season = seasonMatch.name;
+                }
+                const year = parseInt(matches[3], 10) || 'Unknown';
+                return {
+                    season, year
+                }
+            }
+        }
+        else {
+            const standAloneYear = /^(\d+)$/im;
+            const matches = text.match(standAloneYear);
+            if (matches){
+                return {
+                    season: 'Unknown',
+                    year: parseInt(matches[0], 10),
+                }
+            }
+        }
+    }
+}
+
 function cleanPremiered(text){
     if (text.toLowerCase() === 'unknown'){
         return {
@@ -32,6 +77,19 @@ function cleanPremiered(text){
     }
 }
 
+function findSeasonalYear(premiered, aired){
+    let season = 'Unknown';
+    let year = 'Unknown';
+    let result = {};
+    if (premiered !== 'Unknown'){
+         result = cleanPremiered(premiered);
+    }
+    else if (premiered === 'Unknown' && aired !== 'Unknown'){
+         result = cleanAired(aired);
+    }
+    return result || {season, year};
+}
+
 function cleanRating(text){
     if (text === 'UNKNOWN'){
         return text.replace(text, 'Unknown');
@@ -42,14 +100,18 @@ function cleanRating(text){
 }
 
 function cleanDuration(text) {
+    if (text.toLowerCase() === 'unknown') {
+        return text;
+    }
+
     const regex = /(\d+)\s*(hr|min|sec)(?:\s*per ep)?/gi;
     let match;
-    let hours =  0;
-    let minutes =  0;
-    let seconds = 0;
+    let hours =   0;
+    let minutes =   0;
+    let seconds =  0;
 
     while ((match = regex.exec(text)) !== null) {
-        const value = parseInt(match[1],  10);
+        const value = parseInt(match[1],   10);
         const unit = match[2];
         if (unit === 'hr') {
             hours += value;
@@ -59,8 +121,9 @@ function cleanDuration(text) {
             seconds += value;
         }
     }
-    const totalMinutes = hours *  60 + minutes + Math.round(seconds / 60);
-    return totalMinutes || text;
+
+    const totalMinutes = hours *   60 + minutes + Math.round(seconds /  60);
+    return totalMinutes;
 }
 
 
@@ -80,7 +143,7 @@ async function processAnimeData(data){
         type = type.replace("UNKNOWN", 'Unknown');
         episodes = parseInt(episodes, 10) || 0;
         premiered = premiered.replace("UNKNOWN", "Unknown");
-        const {season, year} = cleanPremiered(premiered);
+        const {season , year} = findSeasonalYear(premiered, aired);
         producers = cleanArray(producers);
         licensors = cleanArray(licensors);
         studios = cleanArray(studios);
@@ -231,7 +294,7 @@ async function constructDataFiles() {
     //const animeData = await readFile('entries.json', 'json', 'AnimeEntry');
     const filteredData = filterAnimeData(animeData);
     await writeFile('entries.json', filteredData);
-    const allowedKeys = ['genres', 'type', 'episodes', 'season', 'year', 'status', 'producers', 'licensors', 'studios', 'source', 'duration', 'rating'];
+    const allowedKeys = ['aired', 'genres', 'type', 'episodes', 'season', 'year', 'status', 'producers', 'licensors', 'studios', 'source', 'duration', 'rating'];
     const specialKeys = ['duration', 'rating', 'season', 'year', 'source', 'premiered'];
 
     for (const key of allowedKeys){
