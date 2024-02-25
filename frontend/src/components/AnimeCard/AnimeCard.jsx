072f864ba105fef19738f7310de3b6be06807ec6
vikiru <72267229+vikiru@users.noreply.github.com>
2024-02-25 12:57:18 -0500

    Update anime card layout to be more compact

--- frontend/src/components/AnimeCard/AnimeCard.jsx
+++ frontend/src/components/AnimeCard/AnimeCard.jsx
@@ -14,70 +14,63 @@ const AnimeCard = ({ anime, index }) => {
     return (
         <div
             key={anime.title}
-            className="card grid min-h-full min-w-[50%] max-w-full cursor-default rounded-lg bg-primary p-2 shadow-lg"
+            className="card grid min-h-full min-w-[50%] max-w-full cursor-default rounded-lg bg-primary p-1"
         >
-            <span className="bg-accent-darker badge badge-accent absolute left-2 top-2 rounded-full p-4 text-white">
+            <span className="bg-accent-darker badge badge-accent absolute left-1 top-1 rounded-full p-2 text-white">
                 {index}
             </span>
-            <div className="mx-4 mt-4">
-                <figure>
+            <div className="grid grid-cols-2 gap-2">
+                <div className="mt-6 rounded-lg bg-primary p-2">
                     {!hasError && (
                         <img
                             src={anime.imageURL}
                             alt={`${anime.title} image`}
-                            className="h-[50%] w-[50%] rounded-lg object-contain"
+                            className="object-fit h-46 mb-2 w-full rounded-lg object-contain"
                         />
-                    )}{' '}
-                </figure>
-            </div>
-            <div className="card-body">
-                <h2 className="card-title pb-4 text-2xl font-semibold text-secondary">{anime.title}</h2>
-                <div className="card-actions mb-4">
-                    {anime.genres.map((g) => (
-                        <span className="badge badge-neutral bg-neutral px-4 py-4 text-lg text-primary" key={g}>
-                            {g}
-                        </span>
-                    ))}
-                    {anime.demographics.map((d) => (
-                        <span className="badge badge-neutral bg-neutral px-4 py-4 text-lg text-primary" key={d}>
-                            {d}
-                        </span>
-                    ))}
-                    {anime.themes.map((t) => (
-                        <span className="badge badge-neutral bg-neutral px-4 py-4 text-lg text-primary" key={t}>
-                            {t}
-                        </span>
-                    ))}
+                    )}
                 </div>
-                <div className="grid gap-4 pb-4 lg:grid-cols-2">
-                    <div className="rounded-lg bg-base-200 p-4 shadow-md">
-                        <h2 className="text-lg font-bold text-secondary">Type</h2>
-                        <p className="text-base text-neutral">{anime.type}</p>
+                <div className="mt-4 flex flex-col justify-between rounded-lg bg-primary p-2">
+                    <div>
+                        <h2 className="mb-2 text-lg font-semibold text-secondary">{anime.title}</h2>
+                        <div className="mb-1 flex flex-wrap">
+                            {anime.genres.map((g) => (
+                                <span
+                                    className="badge badge-neutral mb-1 mr-1 bg-neutral p-1 text-xs text-primary"
+                                    key={g}
+                                >
+                                    {g}
+                                </span>
+                            ))}
+                            {anime.demographics.map((d) => (
+                                <span
+                                    className="badge badge-neutral mb-1 mr-1 bg-neutral p-1 text-xs text-primary"
+                                    key={d}
+                                >
+                                    {d}
+                                </span>
+                            ))}
+                        </div>
+                        <div className="mt-2 text-sm text-neutral">
+                            <span className="font-bold">Type:</span> {anime.type}
+                        </div>
+                        <div className="text-sm text-neutral">
+                            <span className="font-bold">Rating:</span> {anime.rating}
+                        </div>
+                        <div className="text-sm text-neutral">
+                            <span className="font-bold">Episodes:</span> {anime.episodes}
+                        </div>
+                        <div className="text-sm text-neutral">
+                            <span className="font-bold">Score:</span> {anime.score} / 10
+                        </div>
                     </div>
-                    <div className="rounded-lg bg-base-200 p-4 shadow-md">
-                        <h2 className="text-lg font-bold text-secondary">Rating</h2>
-                        <p className="text-base text-neutral">{anime.rating}</p>
+                    <div className="mt-2">
+                        <Link
+                            to={`/anime/${anime.id}`}
+                            className="hover:bg-accent-darker btn btn-accent rounded-lg bg-accent px-2 py-1 uppercase text-white"
+                        >
+                            Read more
+                        </Link>
                     </div>
-                    <div className="rounded-lg bg-base-200 p-4 shadow-md">
-                        <h2 className="text-lg font-bold text-secondary">Episodes</h2>
-                        <p className="text-base text-neutral">{anime.episodes === 0 ? 'Unknown' : anime.episodes}</p>
-                    </div>
-                    <div className="rounded-lg bg-base-200 p-4 shadow-md">
-                        <h2 className="text-lg font-bold text-secondary">Score</h2>
-                        <p className="text-base text-neutral">
-                            {anime.score === 0 ? 'Unknown' : `${anime.score} /   10`}
-                        </p>
-                    </div>
-                </div>
-                <div className="card-actions justify-center">
-                    <Link
-                        to={`/anime/${anime.id}`}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="hover:bg-accent-darker btn btn-accent rounded-lg bg-accent px-4 py-2 uppercase text-white"
-                    >
-                        Read more
-                    </Link>
                 </div>
             </div>
         </div>
