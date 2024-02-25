fdf6623d31d71f6e673521cf450cc82d63cd5873
vikiru <72267229+vikiru@users.noreply.github.com>
2024-02-25 11:22:07 -0500

    Cleanup search bar
    - need to make it work better for mobile

--- frontend/src/pages/SearchAnime/SearchAnime.jsx
+++ frontend/src/pages/SearchAnime/SearchAnime.jsx
@@ -1,76 +1,102 @@
-import Fuse from 'fuse.js';
-import { debounce } from 'lodash';
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import MiniSearch from 'minisearch';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { ReactSearchAutocomplete } from 'react-search-autocomplete';
-
-import { cleanTitle } from '../../../../reccomender/utils/fetchData';
 
 const SearchAnime = ({ data, titleIDMap }) => {
-    const items = useMemo(
-        () => titleIDMap.map((item) => ({ id: item.value, name: item.title, synonyms: item.synonyms })),
-        [titleIDMap],
-    );
     const [inputValue, setInputValue] = useState('');
     const [suggestions, setSuggestions] = useState([]);
-    const [selectedItem, setSelectedItem] = useState(null);
+    const [activeSuggestionIndex, setActiveSuggestionIndex] = useState(0);
+    const [showSuggestions, setShowSuggestions] = useState(false);
+    const suggestionRefs = useRef([]);
 
     const navigate = useNavigate();
-    const navigateToAnime = debounce((id) => navigate(`/anime/${id}`), 500);
+    const navigateToAnime = (id) => navigate(`/anime/${id}`);
 
-    const fuse = useMemo(
-        () =>
-            new Fuse(items, {
-                keys: ['name', 'synonyms'],
-                includeScore: true,
-                shouldSort: true,
-                threshold: 1,
-                isCaseSensitive: false,
-                matchAllTokens: false,
-                tokenize: true,
-                limit: 10,
-                preprocess: {
-                    name: cleanTitle,
-                    synonyms: cleanTitle,
-                },
-            }),
-        [items],
-    );
+    const miniSearch = useMemo(() => {
+        const miniSearch = new MiniSearch({
+            fields: ['title', 'synonyms'],
+            storeFields: ['title', 'synonyms'],
+            searchOptions: {
+                boost: { title: 2 },
+                fuzzy: 0.2,
+            },
+        });
+
+        miniSearch.addAll(titleIDMap.map((item) => ({ id: item.value, title: item.title, synonyms: item.synonyms })));
+        return miniSearch;
+    }, [titleIDMap]);
 
     const handleOnSearch = useCallback(
         (string) => {
-            const results = fuse.search(string);
-            setSuggestions(results.map((result) => result.item));
+            const results = miniSearch.search(string, { limit: 10 });
+            setSuggestions(
+                results
+                    .slice(0, 10)
+                    .map((result) => ({ id: result.id, title: result.title, synonyms: result.synonyms })),
+            );
+            setActiveSuggestionIndex(0);
+            setShowSuggestions(true);
         },
-        [fuse],
+        [miniSearch],
     );
 
     const handleOnSelect = useCallback((selectedItem) => {
         setSuggestions([]);
-        setInputValue(selectedItem.name);
+        setInputValue(selectedItem.title);
         navigateToAnime(selectedItem.id);
+        setShowSuggestions(false);
     }, []);
 
-    const formatResult = (item) => (
-        <div className="card cursor-pointer p-2 hover:bg-neutral-300">
-            <div>{item.name}</div>
-        </div>
+    const handleKeyDown = useCallback(
+        (event) => {
+            if (event.key === 'ArrowDown') {
+                setActiveSuggestionIndex((prevIndex) => Math.min(prevIndex + 1, suggestions.length - 1));
+            } else if (event.key === 'ArrowUp') {
+                setActiveSuggestionIndex((prevIndex) => Math.max(prevIndex - 1, 0));
+            } else if (event.key === 'Enter') {
+                handleOnSelect(suggestions[activeSuggestionIndex]);
+            }
+        },
+        [suggestions, activeSuggestionIndex],
     );
 
+    useEffect(() => {
+        if (suggestionRefs.current[activeSuggestionIndex]) {
+            suggestionRefs.current[activeSuggestionIndex].scrollIntoView({ block: 'nearest', behavior: 'smooth' });
+        }
+    }, [activeSuggestionIndex]);
+
     return (
-        <div className="container min-h-screen flex-grow p-8 pb-6">
+        <div className={`container min-h-screen flex-grow p-8 pb-6 ${showSuggestions ? 'overflow-y-hidden' : ''}`}>
             <div className="min-h-[40rem] flex-shrink-0">
-                <ReactSearchAutocomplete
-                    items={suggestions}
-                    onSearch={handleOnSearch}
-                    onSelect={handleOnSelect}
-                    formatResult={formatResult}
-                    maxResults={10}
+                <input
+                    type="text"
+                    value={inputValue}
+                    onChange={(e) => {
+                        setInputValue(e.target.value);
+                        handleOnSearch(e.target.value);
+                    }}
+                    onKeyDown={handleKeyDown}
                     placeholder="Type to filter titles..."
-                    className="text-primary-500 w-full rounded-md border-gray-300 p-2 focus:border-blue-500 focus:outline-none dark:text-current"
-                    autocompleteItemClassName="p-2 hover:bg-neutral cursor-pointer"
-                    autocompleteItemActiveClassName="bg-blue-500 text-white"
+                    className="input input-bordered w-full"
                 />
+                {suggestions.length > 0 && (
+                    <div className="search-results mt-4 max-h-[20rem] space-y-2 overflow-y-hidden">
+                        {suggestions.map((suggestion, index) => (
+                            <div
+                                key={index}
+                                ref={(el) => (suggestionRefs.current[index] = el)}
+                                className={`card cursor-pointer transition-shadow duration-200 hover:shadow-lg ${index === activeSuggestionIndex ? 'bg-blue-100' : ''}`}
+                                onClick={() => handleOnSelect(suggestion)}
+                            >
+                                <div className="card-body">
+                                    <p className="text-md card-title">{suggestion.title}</p>
+                                    <p className="text-sm text-gray-500">{suggestion.synonyms.join(', ')}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
             </div>
         </div>
     );
