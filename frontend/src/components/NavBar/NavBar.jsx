fc440eee0d5fba29a78cc469c22c5da84aa9f6e2
vikiru <72267229+vikiru@users.noreply.github.com>
2024-02-29 14:12:48 -0500

    Fix Nav

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -25,7 +25,7 @@ function NavBar() {
         <div className="navbar bg-base-100">
             <div className="navbar-start">
                 <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden" onClick={toggleDropdown}>
+                    <div tabIndex={0} role="button" className="btn btn-ghost" onClick={toggleDropdown}>
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             className="h-5 w-5"
@@ -88,46 +88,10 @@ function NavBar() {
                     </ul>
                 </div>
             </div>
-            <div className="xs:navbar-center lg:navbar-start">
-                <Link to="/" className="font-logo btn btn-ghost lg:text-xl">
+            <div className="xs:navbar-center">
+                <Link to="/" className="font-logo btn btn-ghost text-xl xl:text-3xl">
                     Kelbrum
                 </Link>
-            </div>
-            <div className="navbar-center z-50 hidden lg:flex">
-                <ul className="menu menu-horizontal px-4">
-                    <li>
-                        <details open={openDropdowns.discover}>
-                            <summary>Discover</summary>
-                            <ul className="p-2">
-                                <li className="p-2">
-                                    <Link to="/anime/top">View top anime</Link>
-                                </li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li>
-                        <details open={openDropdowns.information}>
-                            <summary>Information</summary>
-                            <ul className="p-2">
-                                <li className="p-2">
-                                    <Link to="/anime/genres">View all genres</Link>
-                                </li>
-                                <li className="p-2">
-                                    <Link to="/anime/licensors">View all licensors</Link>
-                                </li>
-                                <li className="p-2">
-                                    <Link to="/anime/studios">View all studios</Link>
-                                </li>
-                                <li className="p-2">
-                                    <Link to="/anime/producers">View all producers</Link>
-                                </li>
-                                <li className="p-2">
-                                    <Link to="/anime/seasons">View all seasons</Link>
-                                </li>
-                            </ul>
-                        </details>
-                    </li>
-                </ul>
             </div>
             <div className="navbar-end">
                 <Link to="/anime/search">
