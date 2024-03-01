e5bc390d24ad75c81c1575fa73e57a190a3d1d4a
vikiru <72267229+vikiru@users.noreply.github.com>
2024-02-29 23:49:05 -0500

    Exclude dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,47 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import topLevelAwait from 'vite-plugin-top-level-await';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        topLevelAwait({
-            // The export name of top-level await promise for each chunk module
-            promiseExportName: '__tla',
-            // The function to generate import names of top-level await promise in each chunk module
-            promiseImportName: (i) => `__tla_${i}`,
-        }),
-    ],
+    plugins: [react()],
     optimizeDeps: {
         include: ['lodash', 'ml-distance', 'minisearch', 'react-infinite-scroller'],
+        exclude: [
+            '@tailwindcss/forms',
+            '@tailwindcss/typography',
+            '@tensorflow/tfjs',
+            '@trivago/prettier-plugin-sort-imports',
+            '@types/node',
+            '@types/react',
+            '@types/react-dom',
+            '@vitejs/plugin-react',
+            'autoprefixer',
+            'cross-env',
+            'csv-parse',
+            'daisyui',
+            'eslint',
+            'eslint-config-prettier',
+            'eslint-plugin-comment-length',
+            'eslint-plugin-import',
+            'eslint-plugin-jsdoc',
+            'eslint-plugin-node',
+            'eslint-plugin-prettier',
+            'eslint-plugin-react',
+            'eslint-plugin-react-hooks',
+            'eslint-plugin-react-refresh',
+            'eslint-plugin-sort-exports',
+            'eslint-plugin-tailwindcss',
+            'ml-kmeans',
+            'papaparse',
+            'postcss',
+            'prettier',
+            'prettier-plugin-jsdoc',
+            'prettier-plugin-tailwindcss',
+            'simple-statistics',
+            'tailwind-scrollbar',
+            'tailwindcss',
+            'vite-plugin-top-level-await',
+        ],
     },
     build: {
         target: 'esnext',
