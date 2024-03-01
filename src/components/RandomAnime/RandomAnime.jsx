814c602e610d7f0bfc40d67b9fe5191823e793c4
vikiru <72267229+vikiru@users.noreply.github.com>
2024-03-01 13:38:50 -0500

    Update RandomAnime.jsx

--- src/components/RandomAnime/RandomAnime.jsx
+++ src/components/RandomAnime/RandomAnime.jsx
@@ -28,7 +28,7 @@ const RandomAnime = ({ anime, allAnime }) => {
 
     return (
         <div className="w-full  bg-secondary pb-8 dark:bg-gray-900">
-            <div className="grid w-full px-4 py-6 grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 3xl:grid-cols-4">
+            <div className="grid w-full px-4 py-6 grid-cols-1 lg:grid-cols-2 3xl:grid-cols-3">
                 {randomAnime.map((anime, index) => (
                     <AnimeItem key={anime.title} anime={anime} index={index} />
                 ))}
