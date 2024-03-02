3c799803a5d72939d23d8c4e79e0f74989795aa5
vikiru <72267229+vikiru@users.noreply.github.com>
2024-03-02 12:55:19 -0500

    Add separate contexts

---
+++ src/context/FilteredDataProvider.jsx
@@ -0,0 +1,71 @@
+import React, { createContext, useContext, useMemo, useRef } from 'react';
+
+import data from '../recommender/data/entries.json';
+import { returnFilteredData } from '../recommender/utils/filter';
+
+const FilteredDataContext = createContext();
+
+const FilteredDataProvider = ({ children }) => {
+    const processedDataRef = useRef({
+        filteredGenres: [],
+        filteredThemes: [],
+        filteredDemographics: [],
+        filteredProducers: [],
+        filteredStudios: [],
+        filteredLicensors: [],
+        filteredSeasons: [],
+        processed: false
+    });
+
+    useEffect(() => {
+        const processData = async () => {
+            const promises = [
+                returnFilteredData(data, 'genres'),
+                returnFilteredData(data, 'themes'),
+                returnFilteredData(data, 'demographics'),
+                returnFilteredData(data, 'producers'),
+                returnFilteredData(data, 'studios'),
+                returnFilteredData(data, 'licensors'),
+                returnFilteredData(data, 'premiered'),
+            ];
+
+            const [
+                filteredGenres,
+                filteredThemes,
+                filteredDemographics,
+                filteredProducers,
+                filteredStudios,
+                filteredLicensors,
+                filteredSeasons,
+            ] = await Promise.all(promises);
+
+            processedData = {
+                filteredGenres,
+                filteredThemes,
+                filteredDemographics,
+                filteredProducers,
+                filteredStudios,
+                filteredLicensors,
+                filteredSeasons,
+                processed: true,
+            };
+        };
+
+        if (!processedData.current.processed) {
+            processData();
+        }
+    }, []);
+
+    const state = useMemo(
+        () => ({
+            ...processedDataRef.current,
+        }),
+        [],
+    );
+
+    return <FilteredDataContext.Provider value={state}>{children}</FilteredDataContext.Provider>;
+};
+
+const useFilteredData = () => useContext(FilteredDataContext);
+
+export {FilteredDataProvider, useFilteredData };
---
+++ src/context/KMeansProvider.jsx
@@ -0,0 +1,22 @@
+import React, { createContext, useContext, useMemo, useRef } from 'react';
+
+import kmeans from '../recommender/data/kmeans.json';
+
+const KMeansContext = createContext();
+
+const KMeansProvider = ({ children }) => {
+    const kmeansRef = useRef(kmeans);
+
+    const state = useMemo(
+        () => ({
+            kmeans: kmeansRef.current,
+        }),
+        [],
+    );
+
+    return <KMeansContext.Provider value={state}>{children}</KMeansContext.Provider>;
+};
+
+const useKMeans = () => useContext(KMeansContext);
+
+export {KMeansProvider, useKMeans };
