88642b9e591d1b11f7d51ace6162f979a0a11c40
vikiru <72267229+vikiru@users.noreply.github.com>
2024-03-03 16:08:29 -0500

    Convert random anime to use react slick
    - additionally use react-intersection-observer

--- src/components/RandomAnime/RandomAnime.jsx
+++ src/components/RandomAnime/RandomAnime.jsx
@@ -1,5 +1,9 @@
 import React, { useMemo } from 'react';
 import { Link } from 'react-router-dom';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { useInView } from 'react-intersection-observer';
 
 import { shuffleRandom } from '../../recommender/recommender';
 import AnimeCard from '../AnimeCard/AnimeCard';
@@ -11,17 +15,48 @@ const RandomAnime = ({ anime, allAnime }) => {
         return shuffled.slice(0, 10);
     }, [allAnime]);
 
+    const [ref, inView] = useInView({
+        triggerOnce: true, 
+    });
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        initialSlide: 1,
+        autoplay: inView, 
+        autoplaySpeed: 1500,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    };
+
     return (
-        <section id="random-anime" className="w-full bg-secondary pb-8 dark:bg-gray-900">
-            <div className="3xl:grid-cols-3 grid w-full grid-cols-1 px-4 py-6 lg:grid-cols-2">
+        <section id="random-anime" className="w-full bg-secondary pb-8 dark:bg-gray-900 overflow-x-hidden" ref={ref}>
+            <Slider {...settings}>
                 {shuffledAnime.map((anime, index) => (
-                    <div key={anime.title} className="m-4 flex flex-col justify-start">
+                    <div key={anime.title} className="m-4 mx-auto flex flex-col justify-between">
                         <AnimeCard anime={anime} index={index + 1} />
                     </div>
                 ))}
-            </div>
+            </Slider>
             {anime !== undefined && (
-                <div className="mt-2 flex items-center justify-center">
+                <div className="mt-2 flex mx-auto items-center justify-center">
                     <Link
                         to={`/anime/recommendations/${anime.id}`}
                         className="hover:bg-accent-darker btn btn-accent rounded-lg bg-accent px-2 py-1 uppercase text-white"
