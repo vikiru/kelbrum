9e31e52ebd7992ac2cf9a88f73be6de43f368521
vikiru <72267229+vikiru@users.noreply.github.com>
2024-03-05 16:21:16 -0500

    Update initializer.js

--- src/recommender/scripts/initializer.js
+++ src/recommender/scripts/initializer.js
@@ -1,23 +1,29 @@
 import * as tf from '@tensorflow/tfjs';
+import nlp from 'compromise';
+import fs from 'fs';
+import { distance, similarity } from 'ml-distance';
 
+import { readJSONFile } from '../dataAccess/readFile.js';
+import { returnKmeansModel, returnOptimalK } from '../dataAccess/train.js';
+import { writeData } from '../dataAccess/writeFile.js';
 import {
-    customDistance,
     retrieveAnimeData,
     returnClusterSimilarities,
     returnRandomRecommendations,
     weightedDistance,
 } from '../recommender.js';
-import { distance, similarity } from 'ml-distance';
-import { returnKmeansModel, returnOptimalK } from '../dataAccess/train.js';
-
 import { createFeatureTensor } from '../utils/normalize.js';
-import fs from 'fs';
 import { initializeDataFile } from '../utils/utils.js';
-import nlp from 'compromise';
-import { readJSONFile } from '../dataAccess/readFile.js';
-import { writeData } from '../dataAccess/writeFile.js';
 
-function testDistances(indexA, indexB, featureArray){
+/**
+ * Calculate various distance metrics between two feature tensors. This function is primarily used to test the
+ * effectiveness of each distance measure
+ *
+ * @param {number} indexA - The index of the first feature tensor in the featureArray
+ * @param {number} indexB - The index of the second feature tensor in the featureArray
+ * @param {Array} featureArray - An array containing the feature tensors
+ */
+function testDistances(indexA, indexB, featureArray) {
     const tensorA = featureArray[indexA];
     const tensorB = featureArray[indexB];
 
@@ -48,11 +54,18 @@ function testDistances(indexA, indexB, featureArray){
     const matusitaDistance = distance.matusita(tensorA, tensorB);
     console.log(`Matusita Distance: ${matusitaDistance}`);
 
+    const manhattanDistanceVal = distance.manhattan(tensorA, tensorB);
+    console.log(`Manhattan Distance: ${manhattanDistanceVal}`);
+
+    const euclideanDistanceVal = distance.euclidean(tensorA, tensorB);
+    console.log(`Euclidean Distance: ${euclideanDistanceVal}`);
+
+    const squaredEuclideanDistanceVal = distance.squaredEuclidean(tensorA, tensorB);
+    console.log(`Squared Euclidean Distance: ${squaredEuclideanDistanceVal}`);
+
     const weightedDistanceVal = weightedDistance(tensorA, tensorB);
     console.log(`Weighted Distance: ${weightedDistanceVal}`);
 }
-
-
 
 async function main() {
     try {
@@ -69,20 +82,25 @@ async function main() {
         // dice/jaccard/gower
         // elfen and zom 100 -> 0, 0, 0.5/0.3/0.16 , 0.4/0.57/0.21, 0, 1/1/0.078, 1/1/0.0358
         // zom 100 and undead unluck -> 0, 0, 0, 0.25/0.4/0.105, 1/1/0.4, 1/1/0.058, 1/1/0.0358
-        const animeOne = data.findIndex(d => d.malID === 54112);
-        const animeTwo = data.findIndex(d => d.malID === 226); // 52741 - uu, 226 - ef
+        const animeOne = data.findIndex((d) => d.malID === 54112); // 54122 - zom 100, 25013- ayona
+        const animeTwo = data.findIndex((d) => d.malID === 52741); // 52741 - uu, 226 - ef. 2890 - ponyo
         //testDistances(animeOne, animeTwo, featureArray);
 
-        //await returnOptimalK(featureArray, 50, weightedDistance, 'weightedDistance.json');
-        const kmeans = await returnKmeansModel(featureArray, 48, weightedDistance);
+        //await returnOptimalK(featureArray, 10, distance.jaccard, 'weightedDistance.json');
+        const kmeans = await returnKmeansModel(featureArray, 10, weightedDistance);
         //await writeData('featureArray.json', featureArray);
-       // await writeData('titleIDMap.json', titleIDMap);
+        // await writeData('titleIDMap.json', titleIDMap);
         //await writeData('kmeans.json', kmeans);
     } catch (err) {
         console.error('Error occured:', err);
     }
 }
 
+// 48, 49 are almost identical but on the right track.
+
 // 190 seems nice but maybe too strict.
 // k = 10, 11, 37, 90, 99
 main();
+
+// assign ordinal values to type, rating, demographics
+//
