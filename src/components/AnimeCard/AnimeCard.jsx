44bb23fffa9c6f608ef83ca189fbd94c34b02d88
vikiru <72267229+vikiru@users.noreply.github.com>
2024-03-05 22:01:28 -0500

    Remove unused import

--- src/components/AnimeCard/AnimeCard.jsx
+++ src/components/AnimeCard/AnimeCard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Img } from 'react-optimized-image';
+
 import { Link } from 'react-router-dom';
 
 const AnimeCard = ({ anime, index }) => {
