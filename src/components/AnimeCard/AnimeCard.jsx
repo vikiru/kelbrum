1ea9ad742b5b8d74db6a12e0b58147de8c6a6e63
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2024-03-07 12:28:08 -0500

    Add style and responsive changes to the UI + adjust recommendation weights and normalization used (#66)
    * Update weights
    
    * Update featureArray.json
    
    * Add year
    
    * Update SearchAnime.jsx
    
    * Update initializer.js
    
    * Fix missing premiered props
    
    * Fix issues with distance function
    
    * Update normalize.js
    
    * Update initializer.js
    
    * Update index.css
    
    * Update SeasonsPage.jsx
    
    * Update normalize.js
    
    * Update InfinitePagination.jsx
    
    * Commit recent changes
    
    - updated search bar to use filter
    - style changes and removed .webp from images
    
    * Commit recent changes
    
    - update styling
    - experiment with different weights and normalizations
    - add a function to get shared indices for genres, themes and synopsis
    
    * Update SearchBar.jsx
    
    * Commit recent changes
    
    * Update entries.json
    
    * Remove infinite scroller from infinite pagination
    
    * Add function return values as they are
    
    - additonally update weights
    
    * Commit recent style changes
    
    * Commit recent changes
    
    * Add live site url to readme
    
    * Commit recent style changes
    
    * Update AnimeCard.jsx
    
    * Commit recent changes

--- src/components/AnimeCard/AnimeCard.jsx
+++ src/components/AnimeCard/AnimeCard.jsx
@@ -4,96 +4,68 @@ import { Link } from 'react-router-dom';
 const AnimeCard = ({ anime, index }) => {
     const excludedURL = 'https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png';
     const [hasError, setHasError] = useState(anime.imageURL === excludedURL);
-    const [webPURL, setWebPURL] = useState(anime.imageURL);
-
-    useEffect(() => {
-        if (!hasError) {
-            setWebPURL(anime.imageURL.replace('.jpg', '.webp'));
-        }
-    }, [anime.imageURL]);
-
-    useEffect(() => {
-        if (!hasError) {
-            const preloadLink = document.createElement('link');
-            preloadLink.href = anime.imageURL;
-            preloadLink.rel = 'preload';
-            preloadLink.as = 'image';
-            document.head.appendChild(preloadLink);
-
-            return () => {
-                document.head.removeChild(preloadLink);
-            };
-        }
-    }, [anime.imageURL, hasError]);
 
     const handleImageError = () => {
         setHasError(true);
     };
 
     return (
-        <section
-            id="anime-card"
-            key={anime.id}
-            className="card mx-auto flex w-[80%] cursor-default flex-col overflow-hidden rounded-lg bg-primary p-1 pb-4 xs:min-h-[60vh] 2xl:w-[70%] dark:bg-gray-800"
-        >
-            <div className="flex min-h-[10vh] items-center justify-center xs:pb-0 lg:pt-4 2xl:pb-2">
-                <h2 className="text-center text-lg font-semibold text-neutral xs:text-sm lg:text-2xl dark:text-gray-100">
-                    {anime.title}
-                </h2>
+        <div className="card mx-auto flex cursor-default flex-col overflow-hidden rounded-lg bg-primary p-4 shadow-lg xs:w-full 2xl:w-[70%] dark:bg-gray-800">
+            <div className="flex flex-grow flex-col">
+                <section id="title" className="flex flex-col items-center justify-center">
+                    <h2 className="text-center text-lg font-semibold text-neutral 4xl:text-4xl dark:text-gray-100">
+                        {anime.title.length > 32 ? anime.title.substring(0, 32) + '...' : anime.title}
+                    </h2>
+                </section>
+
+                <section id="image" className="mt-4 flex justify-center">
+                    {!hasError && (
+                        <img
+                            src={anime.imageURL}
+                            alt={`${anime.title} image`}
+                            className="h-auto w-full rounded-lg object-contain 2xl:h-[500px] 4xl:h-[800px]"
+                            loading="lazy"
+                            onError={handleImageError}
+                        />
+                    )}
+                    {hasError && <div className="h-64 w-full rounded-lg bg-gray-200 dark:bg-gray-700"></div>}
+                </section>
+
+                <section id="genres" className="mt-6 flex flex-wrap items-center justify-center">
+                    {anime.genres
+                        .filter((g) => g !== 'Unknown')
+                        .map((g) => (
+                            <span
+                                className="badge badge-neutral mb-1 mr-1 bg-neutral p-1 text-lg sm:p-4 lg:text-xl 3xl:p-3 3xl:text-xl 4xl:p-4 4xl:text-4xl"
+                                key={g}
+                            >
+                                {g}
+                            </span>
+                        ))}
+
+                    {anime.demographics
+                        .filter((d) => d !== 'Unknown')
+                        .map((d) => (
+                            <span
+                                className="badge badge-neutral mb-1 mr-1 bg-neutral p-1 text-lg sm:p-4 lg:text-xl 3xl:p-3 3xl:text-xl 4xl:p-4 4xl:text-4xl"
+                                key={d}
+                            >
+                                {d}
+                            </span>
+                        ))}
+                </section>
             </div>
 
-            <section id="image" className="flex flex-col gap-2 pb-2">
-                <div className="flex min-h-[50vh] flex-grow justify-center rounded-lg p-2 xs:min-h-[20vh] 2xl:min-h-[20vh]">
-                    {!hasError && (
-                        <picture>
-                            <source srcSet={`${webPURL}`} type="image/webp" />
-                            <source srcSet={`${anime.imageURL}`} type="image/jpeg" />
-                            <img
-                                src={`${anime.imageURL}`}
-                                alt={`${anime.title} image`}
-                                className="xl:h-50 3xl:h-70 w-full rounded-lg object-cover xs:h-32 lg:h-48 4xl:h-72 5xl:h-80"
-                                loading="lazy"
-                                onError={handleImageError}
-                            />
-                        </picture>
-                    )}
-                    {hasError && <div className="w-full rounded-lg bg-gray-200 lg:h-48 dark:bg-gray-700"></div>}
-                </div>
-            </section>
-
-            <section id="genres" className="flex min-h-[10vh] flex-wrap items-center justify-center">
-                {anime.genres
-                    .filter((g) => g !== 'Unknown')
-                    .map((g) => (
-                        <span
-                            className="badge badge-neutral mb-1 mr-1 bg-neutral p-1 text-xs sm:p-2 lg:text-lg"
-                            key={g}
-                        >
-                            {g}
-                        </span>
-                    ))}
-
-                {anime.demographics
-                    .filter((d) => d !== 'Unknown')
-                    .map((d) => (
-                        <span
-                            className="badge badge-neutral mb-1 mr-1 bg-neutral p-1 text-xs sm:p-2  lg:text-lg"
-                            key={d}
-                        >
-                            {d}
-                        </span>
-                    ))}
-            </section>
-            <section id="read-more" className="flex justify-center pt-2">
+            <section id="read-more" className="mt-4 flex justify-center">
                 <Link
                     to={`/anime/${anime.id}`}
-                    className="hover:bg-accent-darker btn btn-accent rounded-lg bg-accent px-2 uppercase text-white xs:px-2 xs:py-1 xs:text-sm 3xl:px-4 3xl:py-2 3xl:text-xl"
+                    className="btn btn-accent rounded-lg bg-accent px-4 py-2 uppercase text-white"
                     aria-label={`Read more about ${anime.title}`}
                 >
                     Read more
                 </Link>
             </section>
-        </section>
+        </div>
     );
 };
 
