37bf95dd5846016271ffb1ec44566e515d23c219
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2026-01-15 01:13:01 -0500

    chore: update deps, migrate to biome, pnpm

---
+++ vite.config.mjs
@@ -0,0 +1,10 @@
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    plugins: [react(), tailwindcss()],
+    build: {
+        sourcemap: false,
+    },
+});
